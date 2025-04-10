679567a27fa3c3032a9ad72b969622309313894a
PKoralewski <pkoralewski1@gmail.com>
2025-04-10 17:18:16 +0200

    feat: add nav with nav link component

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,6 +6,11 @@
     <title>Document</title>
 </head>
 <body>
+    <nav>
+        <x-nav-link href='/'>Home</x-nav-link>
+        <x-nav-link href='/about'>About</x-nav-link>
+        <x-nav-link href='/contact'>Contact</x-nav-link>
+    </nav>
     {{ $slot }}
 </body>
 </html>
